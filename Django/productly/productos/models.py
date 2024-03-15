c6ff96e0d9726e73a8658d0c275674793d2435ac
Aldo Mondragon <rodrigomondragon@outlook.com>
2024-03-15 17:59:56 -0600

    cambiar modelos

--- Django/productly/productos/models.py
+++ Django/productly/productos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Categoria(models.Model):
@@ -9,3 +10,5 @@ class Producto(models.Model):
     stock  = models.IntegerField()
     puntaje = models.FloatField()
     Categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE) 
+
+creado_en = models.DateTimeField(default=timezone.now)
